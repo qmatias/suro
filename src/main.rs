9dd2356e43d00bb8403772a356c4d369b64a02b2
Matias Kotlik <mdkotlik@gmail.com>
2019-05-15 11:17:29 -0500

    add command line arguments for file and verbosity

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::fs;
 
+use clap::{App, Arg};
+
 mod token;
 mod parser;
 mod scope;
@@ -8,12 +10,33 @@ mod interpreter;
 mod builtins;
 
 fn main() {
-    let tokens = token::tokenize(&fs::read_to_string("program.suro").unwrap());
-//    println!("Tokens: {:?}", &tokens);
+    let matches = App::new("Suro Interpreter")
+        .version("0.0.1")
+        .author("mdkotlik (mdkotlik@gmail.com)")
+        .about("Compiler for the suro language")
+        .arg(Arg::with_name("FILE")
+            .help("The file to run")
+            .required(true)
+            .index(1))
+        .arg(Arg::with_name("v")
+            .short("v")
+            .help("Sets verbose mode"))
+        .get_matches();
+
+    let verbose = matches.is_present("v");
+
+    let tokens = token::tokenize(&fs::read_to_string(matches.value_of("FILE").unwrap()).unwrap());
+    if verbose {
+        println!("Tokens: {:?}", &tokens);
+    }
 
     let program = parser::Parser::new(tokens).parse();
-//    println!("Tree: {:#?}", &program);
+    if verbose {
+        println!("Tree: {:#?}", &program);
+    }
 
     let result = interpreter::Interpreter::new().eval_program(&program);
-    println!("Result: {:?}", &result)
+    if verbose {
+        println!("Result: {:?}", &result)
+    }
 }
